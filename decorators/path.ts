59330ad38a549286655a3be061d9d472aefebb67
gyjlovelh <18062791691@163.com>
2018-12-05 11:32:47 +0800

    #before/after#

--- decorators/path.ts
+++ decorators/path.ts
@@ -73,9 +73,7 @@ export const Path = (path: string): Function => {
                 const afterFn = Reflect.getMetadata(afterSymbolKey, target, propertyKey);
                 return httpClient[httpMethod](url, body).pipe(map(res => {
                     if (afterFn) {
-                        return afterFn({
-                            res,
-                        });
+                        return afterFn(res);
                     }
                     return res;
                 }));
