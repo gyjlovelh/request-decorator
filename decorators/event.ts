4421f6fe8ebf12fdd35637770b74e0f0392a304c
gyjlovelh <18062791691@163.com>
2018-12-05 10:21:51 +0800

    #before/after#

--- decorators/event.ts
+++ decorators/event.ts
@@ -8,7 +8,7 @@ export const afterSymbolKey = Symbol.for('common:after');
 const [Before, After] = ['before', 'after'].map(event => {
     return fn => {
         return (target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<Function>) => {
-            Reflect.defineMetadata(beforeSymbolKey, fn, target, propertyKey);
+            Reflect.defineMetadata(event === 'before' ? beforeSymbolKey : afterSymbolKey, fn, target, propertyKey);
         }
     }
 });
